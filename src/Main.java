f0090e2fed73f0380e1e93d7fc69961a0b0654b7
599937128 <599937128@qq.com>
2019-04-10 12:03:27 +0800

    实现n的阶乘使用递归实现

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * 实现n的阶乘使用递归实现
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer num = sc.nextInt();
+        Integer value = getNMethod(num);
+        System.out.println(value);
+    }
+    // 实现n的阶乘
+    public  static Integer getNMethod(Integer n) {
+        if (n < 0) {
+            return null;
+        }
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n*getNMethod(n-1);
+    }
+}
